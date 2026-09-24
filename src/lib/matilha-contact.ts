cc8afa3e02d5d479ac6891d535b69d2b90237298
Raphaeljdk <raphaelfreitasdossantos651@gmail.com>
2026-09-24 20:58:10 -0300

    feat: adicionar rota da Matilha Prado no Waze

--- src/lib/matilha-contact.ts
+++ src/lib/matilha-contact.ts
@@ -3,6 +3,7 @@ export const MATILHA_CONTACT = {
   whatsappNumber: '5511915942356',
   instagram: 'https://www.instagram.com/matilhaprado/',
   google: 'https://share.google/OcE5O53xqxRPYL1oT',
+  waze: 'https://www.waze.com/ul?q=Av.%20%C3%81gua%20Fria%2C%201647%20-%20Santana%2C%20S%C3%A3o%20Paulo%2C%20SP&navigate=yes',
 } as const
 
 export function matilhaWhatsAppUrl(message?: string) {
